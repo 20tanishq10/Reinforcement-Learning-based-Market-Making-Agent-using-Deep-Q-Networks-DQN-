9daf4f9078ef785f1bb9a92aca219e38fd1d0765
20tanishq10 <134104696+20tanishq10@users.noreply.github.com>
2025-06-15 16:35:02 +0530

    Update tensorforce_agent.py

--- agent/tensorforce_agent.py
+++ agent/tensorforce_agent.py
@@ -17,11 +17,11 @@ def get_dueling_dqn_agent(model, environment, max_episode_timesteps, device='cpu
         states=environment.states,
         actions=environment.actions,
         max_episode_timesteps=max_episode_timesteps,
-        network=model,  # or 'auto' if not using custom model
+        network=model,
         memory=10000,
-        target_update_frequency=100,
         double_q_model=True,
         huber_loss=0.0,
         exploration=dict(type='linear', unit='timesteps', num_steps=10000, initial_value=1.0, final_value=0.1),
         device=device
     )
+
