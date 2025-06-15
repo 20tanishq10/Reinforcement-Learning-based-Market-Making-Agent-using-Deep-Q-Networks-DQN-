ee01b9aa65c019045a3ca53e572d3485ebf85328
20tanishq10 <134104696+20tanishq10@users.noreply.github.com>
2025-06-15 16:44:34 +0530

    Update tensorforce_agent.py

--- agent/tensorforce_agent.py
+++ agent/tensorforce_agent.py
@@ -1,22 +1,22 @@
-# tensorforce_agent.py
 from tensorforce import Agent
-from tensorforce.agents import PPOAgent, DQNAgent
 
 def get_ppo_agent(model, environment, max_episode_timesteps, device='cpu', learning_rate=1e-4, horizon=1):
-    return PPOAgent(
-        states=environment.states,
-        actions=environment.actions,
-        network='auto',
-        batch_size=10,
-        learning_rate=1e-3,
-        update_frequency=10,
-        max_episode_timesteps=2000
-        )
+    config = {
+        "agent": "ppo",
+        "network": "auto",
+        "learning_rate": learning_rate,
+        "batch_size": 10,
+        "update_frequency": 10,
+        "max_episode_timesteps": max_episode_timesteps,
+        "device": device
+    }
+    return Agent.create(config=config, environment=environment)
+
 
 def get_dueling_dqn_agent(model, environment, max_episode_timesteps, device='cpu', learning_rate=1e-4, horizon=1):
     config = {
         "agent": "dqn",
-        "network": model,  # or 'auto'
+        "network": model,
         "memory": 10000,
         "update_frequency": 1,
         "learning_rate": learning_rate,
@@ -26,8 +26,4 @@ def get_dueling_dqn_agent(model, environment, max_episode_timesteps, device='cpu
         "max_episode_timesteps": max_episode_timesteps,
         "device": device
     }
-
-    return Agent.create(
-        config=config,
-        environment=environment
-    )
+    return Agent.create(config=config, environment=environment)
