9eb409e9d9c4d875bebd2f712e219ac6c95517e9
20tanishq10 <134104696+20tanishq10@users.noreply.github.com>
2025-06-15 16:40:10 +0530

    Update tensorforce_agent.py

--- agent/tensorforce_agent.py
+++ agent/tensorforce_agent.py
@@ -12,16 +12,22 @@ def get_ppo_agent(model, environment, max_episode_timesteps, device='cpu', learn
         max_episode_timesteps=2000
         )
 
+from tensorforce.agents import DQNAgent
+
 def get_dueling_dqn_agent(model, environment, max_episode_timesteps, device='cpu', learning_rate=1e-4, horizon=1):
     return DQNAgent(
         states=environment.states,
         actions=environment.actions,
-        max_episode_timesteps=max_episode_timesteps,
         network=model,
+        max_episode_timesteps=max_episode_timesteps,
+        update=dict(unit='timesteps', batch_size=32),
         memory=10000,
-        double_q_model=True,
-        huber_loss=0.0,
-        exploration=dict(type='linear', unit='timesteps', num_steps=10000, initial_value=1.0, final_value=0.1),
+        optimizer=dict(type='adam', learning_rate=learning_rate),
+        exploration=dict(
+            type='linear', unit='timesteps', num_steps=10000,
+            initial_value=1.0, final_value=0.1
+        ),
         device=device
     )
+
 
